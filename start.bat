81b8121c68796277b183524111aa839e3489165e
priyanshubuddy <priyanshudubey333@gmail.com>
2024-10-11 17:48:03 +0530

    "Modified start.bat to change IPv4 address extraction and removal of carriage return characters."

--- start.bat
+++ start.bat
@@ -1,11 +1,10 @@
 @echo off
 setlocal
 
-REM Get the IPv4 address
-for /f "tokens=14" %%i in ('ipconfig ^| findstr /r /c:"IPv4 Address"') do set IP=%%i
+for /f "tokens=14 delims= " %%i in ('ipconfig ^| findstr /r /c:"IPv4 Address"') do set IP=%%i
 
 REM Remove any carriage return characters
-set IP=%IP:~0,-1%
+set IP=%IP%
 
 REM Output the IP address to config.js
 echo module.exports = { IP_ADDRESS: "%IP%" }; > Frontend/config.js
